4df66a2fb8e5238ce4da34e8d9bffd22ab49b47d
Khalid Saifullah <khalid@outlook.com.au>
2019-04-24 22:02:18 +1000

    hello world

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
 import * as express from 'express';
 
 const app = express();
+
+app.get('/', function (_: express.Request, res: express.Response) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
