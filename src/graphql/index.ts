fd7e5631397cb3a66018e931b4d3cfcec1388131
Khalid Saifullah <khalid@outlook.com.au>
2019-04-25 13:43:12 +1000

    Structured schema and query

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,50 +1,22 @@
+import * as path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import { Application } from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
-export const addGraphQL = (app: Application, path: string): ApolloServer => {
-  const books = [
-    {
-      title: 'Harry Potter and the Chamber of Secrets',
-      author: 'J.K. Rowling',
-    },
-    {
-      title: 'Jurassic Park',
-      author: 'Michael Crichton',
-    },
-  ];
+export const addGraphQL = (app: Application, uriPath: string): ApolloServer => {
+  // Type definitions.
+  const typeDefs = gql`${mergeTypes(
+    fileLoader(path.join(__dirname, 'schemas/**/*.gql'), { recursive: true }),
+    { all: true },
+  )}`;
 
-  const typeDefs = gql`
-    # Comments in GraphQL are defined with the hash (#) symbol.
+  // Resolver object.
+  const resolvers = mergeResolvers(
+    fileLoader(path.join(__dirname, './resolvers'), {recursive: true})
+  );
 
-    # This "Book" type can be used in other type declarations.
-    type Book {
-      title: String
-      author: String
-    }
-
-    # The "Query" type is the root of all GraphQL queries.
-    # (A "Mutation" type will be covered later on.)
-    type Query {
-      books: [Book]
-    }
-  `;
-
-  // Resolvers define the technique for fetching the types in the
-  // schema.  We'll retrieve books from the "books" array above.
-  const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
-
-  // In the most basic sense, the ApolloServer can be started
-  // by passing type definitions (typeDefs) and the resolvers
-  // responsible for fetching the data for those types.
   const server = new ApolloServer({ typeDefs, resolvers });
-
-  // This `listen` method launches a web-server.  Existing apps
-  // can utilize middleware options, which we'll discuss later.
-  server.applyMiddleware({ app, path });
+  server.applyMiddleware({ app, path: uriPath });
 
   return server;
 };
---
+++ src/graphql/resolvers/Query.ts
@@ -0,0 +1,12 @@
+export const Query = {
+  books: () => [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ],
+};
