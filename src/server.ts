45e3c3e0e69da1ca659b663add3f7200cf42af97
Khalid Saifullah <khalid@outlook.com.au>
2019-04-26 16:21:04 +1000

    Add database container

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
+import * as cors from 'cors';
 import * as express from 'express';
 import { addGraphQL } from './graphql';
 
-const app = express();
-const port = 4000;
+const { PORT = 4000 } = process.env;
 
-app.get('/', function (_: express.Request, res: express.Response) {
+const app = express();
+app.use(cors({origin: true}));
+
+app.get('/', function (_, res: express.Response) {
   res.send('Hello World');
 });
 
 addGraphQL(app, '/graphql');
 
-app.listen({ port }, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}`)
+app.listen(PORT, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}`)
 );
