bef2cdf381c928cf331fe47032d219e24eabae6b
Khalid Saifullah <khalid@outlook.com.au>
2019-05-08 06:13:34 +1000

    Use new authorId column

--- src/types/AddPostInput.ts
+++ src/types/AddPostInput.ts
@@ -1,11 +1,10 @@
 import { Record, String, Static, Partial } from 'runtypes';
 import { PostCategory } from './PostCategory';
-import { UserRefInput } from './UserRefInput';
 
 export const AddPostInput = Record({
   title: String,
   category: PostCategory,
-  author: UserRefInput,
+  authorId: String,
 }).And(Partial({
   body: String,
 }));
--- src/types/UserRefInput.ts
+++
@@ -1,7 +0,0 @@
-import { Record, String, Static } from 'runtypes';
-
-export const UserRefInput = Record({
-  id: String,
-});
-
-export type UserRefInput = Static<typeof UserRefInput>;
