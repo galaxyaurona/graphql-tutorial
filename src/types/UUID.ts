d549c501cfb40f2c6c02fa5fab00765ee5d781fa
Khalid Saifullah <khalid@outlook.com.au>
2019-07-24 10:16:59 +1000

    Packages updated. There is a problem with graphql-toolkit.

--- src/types/UUID.ts
+++ src/types/UUID.ts
@@ -1,6 +1,6 @@
 import { String, Static } from 'runtypes';
 import { isUUID } from 'validator';
 
-export const UUID = String.withConstraint(id => isUUID(id, 4), 'User id is not a valid UUID v4.');
+export const UUID = String.withConstraint(id => isUUID(id, 4), { name: 'UUID v4' });
 
 export type UUID = Static<typeof UUID>;
